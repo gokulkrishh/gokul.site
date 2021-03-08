c325b3690abf1754b23cce1ee2b1bfaa95c8dd31
gokulkrishh <krishnangokul9@gmail.com>
2021-03-08 12:26:47 +0530

    Add new pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,34 +1,25 @@
 import React from 'react';
 
 import Footer from '../components/footer';
-import Header from '../components/header';
 import Layout from '../components/layout';
 import Newsletter from '../components/newsletter';
+import Menu from '../components/menu';
 
 import '../styles/index.css';
 
 const NotFoundPage = () => {
   return (
     <div className="grid">
-      <Header />
+      <Menu />
       <Layout>
-        <title>Not found</title>
-        <h1>Page not found</h1>
-        <h2>
+        <h3>Page not found</h3>
+        <h3>
           <span role="img" aria-label="Pensive emoji">
             😔
           </span>{' '}
           We couldn’t find what you were looking for.
-        </h2>
-        {process.env.NODE_ENV === 'development' ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
+        </h3>
         <br />
-        <Link to="/">Go home</Link>.
       </Layout>
       <Newsletter />
       <Footer />
