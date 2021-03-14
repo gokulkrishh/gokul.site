51ac5c28a0efede9096c24d2fa29b13533cd9af7
gokulkrishh <krishnangokul9@gmail.com>
2021-03-14 22:03:44 +0530

    Add gumroad only in book path

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,14 @@
-import React from 'react';
+import React from "react";
 
-export const onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([<script key="tracking" src="https://gumroad.com/js/gumroad.js" type="text/javascript" async />]);
+export const onRenderBody = ({ setHeadComponents, pathname }) => {
+  if (pathname === "/book") {
+    setHeadComponents([
+      <script
+        key="tracking"
+        src="https://gumroad.com/js/gumroad.js"
+        type="text/javascript"
+        async
+      />,
+    ]);
+  }
 };
